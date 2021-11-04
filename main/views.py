3aba09c15b30a672ff60685a8e46ab535b04cbb5
Andrey Pryadko <mrstr91@gmail.com>
2021-11-05 00:03:53 +0200

    template engine

--- main/views.py
+++ main/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 from .models import Product
+from .models import Category
 
 def index (request):
     #context = Product.objects.all()
@@ -9,24 +10,33 @@ def index (request):
 
 
 def pizza (request):
-    context = Product.objects.order_by('created')[:8]
-    return render(request,'main/pizza.html',{'context':context} )
-
-
-def discount (request):
-    return render(request,'main/discount.html' )
+    context = Product.objects.all()
+    category = Category.objects.get(pk = 1)
+    return render(request,'main/pizza.html',{'context':context, 'category': category})
 
 def sushi (request):
-    return render(request,'main/sushi.html' )
+    context = Product.objects.all()
+    category = Category.objects.get(pk = 3)
+    return render(request,'main/sushi.html',{'context':context, 'category': category})
 
 def drink (request):
-    return render(request,'main/drink.html' )
+    context = Product.objects.all()
+    category = Category.objects.get(pk = 4)
+    return render(request,'main/drink.html',{'context':context, 'category': category})
 
 def cart (request):
-    return render(request,'main/cart.html' )
+    context = Product.objects.all()
+    category = Category.objects.get(pk = 1)
+    return render(request,'main/cart.html',{'context':context, 'category': category})
 
 def burger (request):
-    return render(request,'main/burger.html' )
+    context = Product.objects.all()
+    category = Category.objects.get(pk = 2)
+    return render(request,'main/burger.html',{'context':context, 'category': category})
+
+def discount (request):
+    context = Product.objects.all()
+    return render(request,'main/discount.html',{'context':context} )
 
 
 
